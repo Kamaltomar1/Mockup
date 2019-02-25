dd8a6d6b7959b4be315c2954cacab3fb02d28f6c
Kamal Tomar <kamaltomarbca171998@gmail.com>
2019-02-24 21:18:20 -0800

    TEXT VIEW

---
+++ app/src/main/java/mockup1/mockup/mockup/MainActivity.java
@@ -0,0 +1,13 @@
+package mockup1.mockup.mockup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
